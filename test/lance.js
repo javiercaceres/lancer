b85caf56badfba32fad1e716913fa17e66ecd518
Javier Caceres <jcaceresm@nectia.com>
2017-05-28 22:26:23 -0400

    ADD: Tests manipulación de representación jQuery
    Signed-off-by: Javier Caceres <jcaceresm@nectia.com>

--- test/lance.js
+++ test/lance.js
@@ -1,4 +1,4 @@
-var assert = require('chai').assert,
+var expect = require('chai').expect,
     jQuery = require('jquery'),
     jsdom = require('jsdom').jsdom,
     lance = require('../dist/lance.js');
@@ -7,11 +7,39 @@ global.document = jsdom('');
 global.window = document.defaultView;
 global.$ = jQuery(window);
 
-describe('Reactor', function() {
-    describe('get$', function() {
-        it('Debe retornar un objecto jQuery', function() {
-            var helloWorld = lance.r( '<div>{text}</div>', { text: 'Hello world!' } );  
-            assert.equal(helloWorld.get$().constructor.name == 'jQuery', 1);
+describe('Reactor - DOM representation', function () {
+    describe('#get$()', function () {
+        it('should return a jQuery object', function () {
+            var helloWorld = lance.r('<div>{text}</div>', { text: 'Hello world!' });
+            
+            expect(helloWorld.get$().constructor.name).to.equal('jQuery');
+        });
+    });
+
+    describe('#render()', function () {
+        it('should return a updated jQuery object using a map', function () {
+            var helloWorld = lance.r('<div>{text}</div>', { text: 'Hello world!' });
+            var updated = helloWorld.render({ text: 'Bye bye!' });
+            
+            expect(updated.constructor.name).to.equal('jQuery');
+            expect(updated.text()).to.equal('Bye bye!');
+        });
+    });
+
+    describe('#remove()', function () {
+        it('should remove the jQuery representation', function () {
+            var helloWorld = lance.r('<div>{text}</div>', { text: 'Hello world!' });
+            helloWorld.remove();
+            
+            expect(helloWorld.get$()).to.be.null;
+        });
+    });
+
+    describe('#getHtml()', function () {
+        it('should return a HTML DOM Element object', function () {
+            var helloWorld = lance.r('<div>{text}</div>', { text: 'Hello world!' });
+            
+            expect(helloWorld.getHtml()).to.have.property('tagName');
         });
     });
 });
